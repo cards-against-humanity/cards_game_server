ce0fd55047236524a705318cd85952d26ecf0fe5
Thomas Volk <tvolk131@gmail.com>
2017-10-19 00:52:57 -0700

    Game struct and method headers are complete

--- gamelist/game/main.go
+++ gamelist/game/main.go
@@ -2,21 +2,41 @@ package game
 
 import (
 	"time"
+
+	"../../card"
+	"../../user"
 )
 
 // Game - A cards game
 type Game struct {
-	name         string
-	maxPlayers   int
-	players      []player
+	Name         string
+	MaxPlayers   int
+	Players      []Player
+	ownerID      int
+	judgeID      int
 	stage        int
 	nextStage    time.Time
-	whiteDraw    deck
-	whiteDiscard deck
+	whiteDraw    []card.WhiteCard
+	whiteDiscard []card.WhiteCard
+	BlackDraw    []card.BlackCard
+	BlackDiscard []card.BlackCard
+	BlackCurrent card.BlackCard
+}
+
+// CreateGame .
+func CreateGame(name string, maxPlayers int, whiteCards []card.WhiteCard, blackCards []card.BlackCard) Game {
+	return Game{Name: name, MaxPlayers: maxPlayers, Players: []Player{}}
 }
 
 // UserState - The state of a game for a particular user
 type UserState struct {
+	Name         string           `json:"name"`
+	BlackCurrent card.BlackCard   `json:"blackCard"`
+	WhiteCards   []card.WhiteCard `json:"whiteCards"`
+	JudgeID      int              `json:"judgeId"`
+	OwnerID      int              `json:"ownerId"`
+	Players      []Player         `json:"players"`
+	Hand         []card.WhiteCard `json:"hand"`
 }
 
 // GetState returns the game state for a particular user
@@ -24,12 +44,17 @@ func (g Game) GetState(uid int) UserState {
 	return UserState{}
 }
 
-func (g Game) addUser(uid int) {
+// Join .
+func (g Game) Join(u user.User) {
+	// Check UserID
+	// TODO - Assign owner if first player
 }
 
-func (g Game) removeUser(uid int) {
+// Leave .
+func (g Game) Leave(uid int) {
 }
 
-func (g Game) playCard(uid int) error {
+// PlayCard .
+func (g Game) PlayCard(uid int, cid int) error {
 	return nil
 }
