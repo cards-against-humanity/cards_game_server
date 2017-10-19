2be4bf86efdb23040f0dee74bbf19e8b51c41501
Thomas Volk <tvolk131@gmail.com>
2017-10-18 22:07:15 -0700

    Has player struct

--- gamelist/game/player.go
+++ gamelist/game/player.go
@@ -1,9 +1,17 @@
 package game
 
-import "../../user"
+import (
+	"../../card"
+	"../../user"
+)
 
-type player struct {
-	user  user.User
-	hand  deck
-	score int
+// Player a user that also contains other game-specific player data
+type Player struct {
+	user.User `json:"user"`
+	Hand      []card.WhiteCard
+	Score     int `json:"score"`
+}
+
+func newPlayer(user user.User) Player {
+	return Player{User: user, Hand: []card.WhiteCard{}, Score: 0}
 }
