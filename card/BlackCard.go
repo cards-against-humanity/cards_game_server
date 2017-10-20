d0e0a3ad98003139da1307f41c7de66190072eb8
Thomas Volk <tvolk131@gmail.com>
2017-10-19 23:42:10 -0700

    Black and white cards don't have print functions

--- card/BlackCard.go
+++ card/BlackCard.go
@@ -1,7 +1,6 @@
 package card
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -13,16 +12,4 @@ type BlackCard struct {
 // CreateBlackCard generates a card struct
 func CreateBlackCard(id int, text string, answerFields int, createdAt time.Time, updatedAt time.Time, cardpackID int) BlackCard {
 	return BlackCard{Card: Card{ID: id, Type: "black", Text: text, AnswerFields: answerFields, CreatedAt: createdAt, UpdatedAt: updatedAt, CardpackID: cardpackID}}
-}
-
-// Print prints card to console
-func (c BlackCard) Print() {
-	fmt.Println("------------")
-	fmt.Printf("ID: %v\n", c.ID)
-	fmt.Printf("Text: '%v'\n", c.Text)
-	fmt.Printf("Answer Fields: %v\n", c.AnswerFields)
-	fmt.Printf("Created on %v\n", c.CreatedAt.Format(time.UnixDate))
-	fmt.Printf("Updated on %v\n", c.UpdatedAt.Format(time.UnixDate))
-	fmt.Printf("Cardpack ID: %v\n", c.CardpackID)
-	fmt.Println("------------")
 }
--- card/WhiteCard.go
+++ card/WhiteCard.go
@@ -1,7 +1,6 @@
 package card
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -13,15 +12,4 @@ type WhiteCard struct {
 // CreateWhiteCard generates a card struct
 func CreateWhiteCard(id int, text string, createdAt time.Time, updatedAt time.Time, cardpackID int) WhiteCard {
 	return WhiteCard{Card: Card{ID: id, Type: "white", Text: text, CreatedAt: createdAt, UpdatedAt: updatedAt, CardpackID: cardpackID}}
-}
-
-// Print prints card to console
-func (c WhiteCard) Print() {
-	fmt.Println("------------")
-	fmt.Printf("ID: %v\n", c.ID)
-	fmt.Printf("Text: '%v'\n", c.Text)
-	fmt.Printf("Created on %v\n", c.CreatedAt.Format(time.UnixDate))
-	fmt.Printf("Updated on %v\n", c.UpdatedAt.Format(time.UnixDate))
-	fmt.Printf("Cardpack ID: %v\n", c.CardpackID)
-	fmt.Println("------------")
 }
