c7f6068d02d1550b934852a5286cf3dbeb17c8df
Tommy Volk <tvolk131@gmail.com>
2017-10-29 23:26:00 -0500

    Card struct omits answerFields property from JSON if it doesn't exist

--- card/card.go
+++ card/card.go
@@ -9,7 +9,7 @@ type Card struct {
 	ID           int       `json:"id"`
 	Type         string    `json:"type"`
 	Text         string    `json:"text"`
-	AnswerFields int       `json:"answerFields"`
+	AnswerFields int       `json:"answerFields,omitempty"`
 	CreatedAt    time.Time `json:"createdAt"`
 	UpdatedAt    time.Time `json:"updatedAt"`
 	CardpackID   int       `json:"cardpackId"`
