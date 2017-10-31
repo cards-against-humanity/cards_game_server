0b969aa3d692e2a3d5e7a7f5a5ed6639a10aa67f
Tommy Volk <tvolk131@gmail.com>
2017-10-31 17:01:06 -0500

    Game has private and public player structs

--- gamelist/game/player.go
+++ gamelist/game/player.go
@@ -8,10 +8,11 @@ import (
 // Player a user that also contains other game-specific player data
 type Player struct {
 	user.User `json:"user"`
-	Hand      []card.WhiteCard
 	Score     int `json:"score"`
 }
 
-func newPlayer(user user.User) Player {
-	return Player{User: user, Hand: []card.WhiteCard{}, Score: 0}
+type player struct {
+	user  user.User
+	hand  []card.WhiteCard
+	score int
 }
