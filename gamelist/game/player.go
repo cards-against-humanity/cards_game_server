24deddc0f58eb0e2336d8467012a05dfdaecbe82
Thomas Volk <tvolk131@gmail.com>
2017-12-20 15:51:04 -0600

    Transferred some helper functions to player file

--- gamelist/game/player.go
+++ gamelist/game/player.go
@@ -1,6 +1,8 @@
 package game
 
 import (
+	"errors"
+
 	"../../card"
 	"../../user"
 )
@@ -16,4 +18,33 @@ type player struct {
 	user  user.User
 	hand  []card.WhiteCard
 	score int
+}
+
+func (g Game) getPrivatePlayer(pID int) (player, error) {
+	for _, p := range g.Players {
+		if p.user.ID == pID {
+			return p, nil
+		}
+	}
+	return player{}, errors.New("User is not in this game")
+}
+
+func (g Game) getPublicPlayer(pID int) (Player, error) {
+	pPriv, err := g.getPrivatePlayer(pID)
+	if err != nil {
+		return Player{}, err
+	}
+	return Player{User: pPriv.user, Score: pPriv.score, HasPlayed: g.userHasPlayed(pID)}, nil
+}
+
+func (g Game) getPublicPlayerFromPrivate(pPriv player) Player {
+	return Player{User: pPriv.user, Score: pPriv.score, HasPlayed: g.userHasPlayed(pPriv.user.ID)}
+}
+
+func (g Game) getPublicPlayers() []Player {
+	pl := []Player{}
+	for _, p := range g.Players {
+		pl = append(pl, g.getPublicPlayerFromPrivate(p))
+	}
+	return pl
 }
